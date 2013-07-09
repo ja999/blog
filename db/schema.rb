f675d177e60dde8e60fd1f2549fccb87ca344553
ja999 <ja999@vp.pl>
2013-07-09 02:52:25 +0200

    Added admin users, validation for actions

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130708121436) do
+ActiveRecord::Schema.define(version: 20130708140230) do
 
   create_table "comments", force: true do |t|
     t.string   "commenter"
@@ -32,8 +32,8 @@ ActiveRecord::Schema.define(version: 20130708121436) do
   end
 
   create_table "users", force: true do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
+    t.string   "email",                  default: "",    null: false
+    t.string   "encrypted_password",     default: "",    null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
@@ -44,6 +44,7 @@ ActiveRecord::Schema.define(version: 20130708121436) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.boolean  "admin",                  default: false
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
